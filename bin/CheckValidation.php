e3af693cba97ddd44a106881f3b82177b88e1d27
Pavel Prilutskiy <prilutskiy.p@paymaxi.com>
2018-09-13 17:56:24 +0300

    add schemas, data, validator

---
+++ bin/CheckValidation.php
@@ -0,0 +1,24 @@
+<?php
+
+ use JsonSchema\Validator;
+ use JsonSchema\Constraints\Constraint;
+
+
+$loader = require __DIR__.'/../vendor/autoload.php';
+
+$data = ['currencies', 'vendors', 'payout_methods', 'payment_methods', 'payment_methods', 'providers'];
+
+$validator = new Validator;
+foreach ($data as $name) {
+    $file = json_decode(file_get_contents('./data/'. $name .'.json'));
+    $validator->validate($file, (object)['$ref' => 'file://' . realpath('./schemas/' . $name . '_schema.json')], Constraint::CHECK_MODE_APPLY_DEFAULTS);
+
+    if ($validator->isValid()) {
+        echo "The " . $name ." validates against the schema.\n";
+    } else {
+        echo $name . "shema does not validate. Violations:\n";
+        foreach ($validator->getErrors() as $error) {
+            echo sprintf("[%s] %s\n", $error['property'], $error['message']);
+        }
+    }
+}
