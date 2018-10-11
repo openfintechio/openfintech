51951928912a9306f5bba9e250fd0fd6e28e8e67
Dmytro Dzubenko <dmytro@dzubenko.name>
2018-10-11 17:01:27 +0300

    Fix git commands

--- etc/deploy.sh
+++ etc/deploy.sh
@@ -1,8 +1,29 @@
 #!/usr/bin/env bash
 
 set -ex
-mkdir -p metadata && cd metadata && php ../etc/metadata-build.php && cd - && mv metadata /tmp/ \
-&& cd /tmp \
-&& git clone https://${GH_TOKEN}@github.com/paycoreio/openfintech-meta.git  \
-&& cp metadata/* openfintech-meta/data/ \
-&& cd openfintech-meta && ./etc/push.sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_files() {
+  git add .
+  git commit --message "Travis build: ${TRAVIS_BUILD_NUMBER}"
+}
+
+push_files() {
+  git push origin master
+}
+
+build_meta() {
+  mkdir -p metadata && cd metadata && php ../etc/metadata-build.php && cd - && mv metadata /tmp/ && cd /tmp \
+  && git clone https://${GH_TOKEN}@github.com/paycoreio/openfintech-meta.git  \
+  && cp metadata/* openfintech-meta/data/ \
+  && cd openfintech-meta
+}
+
+build_meta
+setup_git
+commit_files
+push_files
