ca1ec5fbcd0a02f6804427e27ef47b684ac25cf5
Dmytro Dzubenko <dmytro@dzubenko.name>
2018-10-11 16:56:45 +0300

    Fix path

--- etc/deploy.sh
+++ etc/deploy.sh
@@ -5,6 +5,4 @@ mkdir -p metadata && cd metadata && php ../etc/metadata-build.php && cd - && mv 
 && cd /tmp \
 && git clone https://${GH_TOKEN}@github.com/paycoreio/openfintech-meta.git  \
 && cp metadata/* openfintech-meta/data/ \
-&& cd openfintech-meta && ./push.sh
-
-aws s3 sync resources s3://resources.openfintech.io
+&& cd openfintech-meta && ./etc/push.sh
