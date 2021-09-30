515286e4b5cf6c94ba43e58028e9750fbc973135
Vitaliy Furman <vitaliy.f@corefy.com>
2021-09-30 11:47:53 +0300

    Upgrade GitHub Actions run number

--- etc/deploy-docs.sh
+++ etc/deploy-docs.sh
@@ -13,7 +13,7 @@ git config --global user.email "actions@github.com"
 git config --global user.name "Github Actions"
 git add .
 if ! (git status | grep -q "nothing to commit"); then
-  git commit --message "Github actions build: ${BUILD_NUMBER}"
+  git commit --message "Github action #${GITHUB_RUN_NUMBER}: ID ${GITHUB_RUN_ID}"
   git push origin master
 fi
 
